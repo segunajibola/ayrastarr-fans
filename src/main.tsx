3081596ae1865287641cadd33aec6652b97eaa2c
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-09 15:49:07 +0100

    addnav

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-cztk6zew1t53fo6u.us.auth0.com"
+      clientId="tkMgPR4Ip3BGyH1FYbvvGISRarhCmw3u"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
