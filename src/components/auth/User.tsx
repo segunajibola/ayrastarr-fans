62ad87a958ddb412840fe0cb8ea9633412385ee1
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-10 02:12:24 +0100

    addnav

--- src/components/auth/User.tsx
+++ src/components/auth/User.tsx
@@ -1,14 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const User = () => {
-const { user, isAuthenticated } = useAuth0()
- /* {user?.picture && <img src={user.picture} alt={user?.name} /> } */
+const { user, isAuthenticated }  = useAuth0()
 
-  return (
-    <>
+return (
+  <>
       {isAuthenticated && (
         <span className="inline-flex items-center justify-center gap-1 px-2.5 py-2 text-sm capitalize bg-pink-500 rounded-full shadow-md bg-melo-sky-blue">
           {user?.nickname}
+          {user?.picture && <img src={user.picture} alt={user?.name} className="w-4 h-4 rounded-full shadow-lg" /> }
         </span>
       )}
     </>
